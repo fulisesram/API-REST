9c247b8099a7e5500d6766c589f2bb81765f8853
Fabian Ulises <fabian-ulises.ramirez@atos.net>
2022-03-28 16:28:03 -0600

    usando inmutabilidad en listas, sets y maps

--- src/main/java/com/example/controller/Controlador.java
+++ src/main/java/com/example/controller/Controlador.java
@@ -30,5 +30,8 @@ public class Controlador {
         return personas;
     }
 
+
+
+
 }
 
--- src/main/java/com/example/controller/main.java
+++ src/main/java/com/example/controller/main.java
@@ -1,20 +1,43 @@
 package com.example.controller;
 
+import com.example.entity.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import com.example.service.IPersonaServ;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
 public class main {
 
-       @Autowired
-       @Qualifier("ImplementacionTestQualifier")
-       static IPersonaServ personaServ;
+//       @Autowired
+//       @Qualifier("ImplementacionTestQualifier")
+//       static IPersonaServ personaServ;
 
            public static void main(String[] args) {
+               //String algo = personaServ.getAlgunMetodo();
+               //System.out.println(algo);
 
-               String algo = personaServ.getAlgunMetodo();
+               Set<Integer> integerSet = new HashSet<>();
+               integerSet.add(1);
+               integerSet.add(2);
+               integerSet.add(3);
+               
+               Set<Integer> inmutableSet = integerSet.
+                       stream().collect(collectingAndThen(
+                               toSet(), Collections::unmodifiableSet));
 
-               System.out.println(algo);
+               System.out.println("inmutableSet = " + inmutableSet);
+
+               integerSet.add(4);
+
+               System.out.println("inmutableSet = " + inmutableSet);
+
 
     }
 }
